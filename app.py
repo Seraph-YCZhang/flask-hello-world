bc1d4a3b85e8a041300bdfc2c6a57cb555698054
Yuchen Zhang <yuchen.zhang@bytedance.com>
2023-12-02 19:59:30 -0800

    add two endpoints

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
 from flask import Flask
+from openai import OpenAI
+
 app = Flask(__name__)
+
+OPEN_AI_KEY  = "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"
+
+client = OpenAI(
+    api_key=OPEN_AI_KEY
+)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    ]
+    )
+    msg =  completion.choices[0].message.content
+    print('msg', completion)
+    return msg
+
+
+
+@app.route('/image')
+def gen_image():
+    response = client.images.generate(
+    model="dall-e-3",
+    prompt="ambience image, cozy, warm, no people, outdoor",
+    size="1792x1024",
+    quality="standard",
+    n=1,
+    )
+    print('image', response.data)
+    image_url = response.data[0].url
+    return image_url
+
+
